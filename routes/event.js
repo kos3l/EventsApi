9adcc55cc480138418f6bb9906bac4788c855bad
kos3l <kk@grumsendev.com>
2023-02-06 18:08:49 +0100

    added last routes to make full crud for event

--- routes/event.js
+++ routes/event.js
@@ -30,6 +30,19 @@ router.get("/", (req, res) => {
     });
 });
 
+// from tutorial - example of get by specific param
+// route: /api/event/:inStock
+// router.get("/inStock", (req, res) => {
+//   event
+//     .find({ inStock: true })
+//     .then((data) => {
+//       res.send(data);
+//     })
+//     .catch((err) => {
+//       res.status(500).send({ message: err.message });
+//     });
+// });
+
 // route: /api/event/:id
 router.get("/:id", (req, res) => {
   event
@@ -43,6 +56,44 @@ router.get("/:id", (req, res) => {
 });
 
 // PUT
-// DELETE
+// route: /api/event/:id
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  event
+    .findByIdAndUpdate(id, req.body)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message:
+            "Cannot update event with id=" + id + ". Event was not found",
+        });
+      } else {
+        res.send({ message: "Event was succesfully updated." });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "Error updating event with id" + id });
+    });
+});
 
+// DELETE
+// route: /api/event/:id
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  event
+    .findByIdAndDelete(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message:
+            "Cannot delete event with id=" + id + ". Event was not found",
+        });
+      } else {
+        res.send({ message: "Event was succesfully delete." });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "Error deleting event with id" + id });
+    });
+});
 module.exports = router;
