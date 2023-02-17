288b82e534b6275cc795a18fdfd5001bd34576c5
kos3l <kk@grumsendev.com>
2023-02-17 14:20:19 +0100

    finished making login and register

--- routes/auth.js
+++ routes/auth.js
@@ -1,29 +1,70 @@
 const router = require("express").Router();
-const user = require("../models/user");
-const { registerValidation } = require("../validation");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { registerValidation, loginValidation } = require("../validation");
 // CRUD
 
 // POST
-// route: /api/register/
+// route: /api/user/register/
 router.post("/register", async (req, res) => {
   const { error } = registerValidation(req.body);
   if (error) {
     return res.status(400).json({ error: error.details[0].message });
   }
-  return res.status(200).json({ msg: "Success!" });
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) {
+    return res.status(400).json({ error: "Email already exists" });
+  }
+  const salt = await bcrypt.genSalt(10);
+  const password = await bcrypt.hash(req.body.password, salt);
+
+  const userObject = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password,
+  });
+
+  try {
+    const savedUser = await userObject.save();
+    res.json({ error: null, data: savedUser._id });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 });
 
-// route: /api/login/
+// route: /api/user/login/
 router.post("/login", async (req, res) => {
-  //   data = req.body;
-  //   user
-  //     .insertMany(data)
-  //     .then((data) => {
-  //       res.send(data);
-  //     })
-  //     .catch((err) => {
-  //       res.status(500).send({ message: err.message });
-  //     });
+  const { error } = loginValidation(req.body);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return res.status(400).json({ error: "Email is wrong" });
+  }
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) {
+    return res.status(400).json({ error: "Wrong password" });
+  }
+
+  const token = jwt.sign(
+    // payload
+    {
+      name: user.name,
+      id: user._id,
+    },
+    // TOKEN_SECRET
+    process.env.TOKEN_SECRET,
+    // EXPIRATION TIME
+    { expiresIn: process.env.JWT_EXPIRES_IN }
+  );
+
+  res.header("auth-token", token).json({
+    error: null,
+    data: { token },
+  });
 });
 
 module.exports = router;
--- validation.js
+++ validation.js
@@ -9,4 +9,12 @@ const registerValidation = (data) => {
   return schema.validate(data);
 };
 
-module.exports = { registerValidation };
+const loginValidation = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().min(6).max(255).required(),
+    password: Joi.string().min(6).max(255).required(),
+  });
+  return schema.validate(data);
+};
+
+module.exports = { registerValidation, loginValidation };
