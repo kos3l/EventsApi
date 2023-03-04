8bc488604024a6f95298afef0d82cdbff892b7f4
kos3l <kos3l@wp.pl>
2023-03-04 18:26:25 +0100

    a

--- services/Event.service.ts
+++ services/Event.service.ts
@@ -14,7 +14,7 @@ const Event: HydratedDocument<
 const createNewEvent = async (
   newEvent: ICreateEventDTO
 ): Promise<HydratedDocument<EventDocument>> => {
-  console.log("abcddaa");
+  console.log("abcddaaaa");
   const createdEvent: HydratedDocument<EventDocument> = await Event.create(
     newEvent
   );
